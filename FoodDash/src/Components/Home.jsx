25cfbf06d4da30203e4f4158757a6dfdad3d43d3
itzel <itzelalwarafii@gmail.com>
2024-03-05 14:30:12 -0800

    navbar

--- FoodDash/src/Components/Home.jsx
+++ FoodDash/src/Components/Home.jsx
@@ -1,12 +1,8 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
+
 export default function Home() {
 
-    return(
-<div className="Home">
-
-        <h1>HOME </h1>
-       
-        
-        </div>
-       
-    )
 }
