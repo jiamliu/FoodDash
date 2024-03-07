da509a434283883d3f9d3b012c33872e6153569b
itzel <itzelalwarafii@gmail.com>
2024-03-06 19:18:23 -0800

    project

--- FoodDash/src/Components/Home.jsx
+++ FoodDash/src/Components/Home.jsx
@@ -1,13 +1,32 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
 
+import axios from "axios"
+import { useEffect, useState ,useContext} from "react"
+ import MealsList  from  './MealsList'
+import MealPageText from './MealPageText'
+import DataContext from "../DataContext"
+import { Link } from "react-router-dom"
+import ImageGallery from "./ImageGallery"
 
 
 export default function Home() {
-return(
-  
-        <div className="Home">
-          <h1>HOME </h1>
-  </div>
 
+  const { searchQuery, setSearchQuery } = useContext(DataContext)
+
+  useEffect(() => {
+    console.log(searchQuery)
+}, [searchQuery])
+
+
+
+return(
+  <>
+       {!searchQuery && <ImageGallery />}
+      {searchQuery && (
+        <div className="container">
+          <MealPageText />
+       
+        </div>
+      )}
+</>
+)
 }
--- FoodDash/src/DataContext.jsx
+++ FoodDash/src/DataContext.jsx
@@ -1,3 +1,8 @@
-import React from "react"
-const DataContext = React.createContext()
+import { createContext } from "react"
+
+
+const DataContext = createContext(undefined)
+
+
+
 export default DataContext 
